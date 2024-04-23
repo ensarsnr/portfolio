35b058114d11f55f226a490738f2e01e2f7456a4
ensarsnr <dev.anise@outlook.com>
2024-04-23 20:42:16 +0300

    init

---
+++ src/constants/Colors.js
@@ -0,0 +1,8 @@
+export default {
+    THROAT: "#291C3A",
+    RUSSIAN_VIOLET: "#331c52",
+    LILAC_SPRING: "#8121d0",
+    PLUMS_THE_WORD: "#dccfed",
+    AQUIFER: "#e1ebed",
+    WHITE: "#ffffff",
+}
